b3e2a78d1520146546c48ca7e99c8547b196aed5
Jakub <jakub.rozi.rozak@gmail.com>
2022-01-17 01:46:25 +0000

    creating events with tickets in them

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-#this is for the json convertable class Todo
+#this is for the json convertable class Event
 from dataclasses import dataclass
 from flask_cors import CORS
+import uuid
 
 
 app = Flask(__name__)
@@ -12,14 +13,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 #dataclass decorateor for easy turning 
 @dataclass
-class Todo(db.Model):
+class Event(db.Model):
+    #these are for @dataclass object!!
     id: int
-    content: str
+    name: str
     date_created: str
+    tickets: object
     
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    tickets = db.relationship('Ticket', backref='event')
+    
+@dataclass
+class Ticket(db.Model):
+    id: int
+    uuid: str
+    owner_id: str
+    
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(120), unique=True, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    def __repr__(self):
+        return '<Pet %r>' % self.owner_id
 
 
 
@@ -27,21 +43,22 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         data = request.get_json()
-        new_task = Todo(content=data['content'])
+        new_event = Event(name=data['name'])
         try:
-            db.session.add(new_task)
+            db.session.add(new_event)
+            for i in range(1,data['amount']):
+                db.session.add(Ticket(uuid=str(uuid.uuid4()), event=new_event))
             db.session.commit()
             return data
         except:
             return 'There was an issue adding your task'
 
     else:
-        tasks = Todo.query.all()
-        return jsonify(Todo.query.all())
+        return jsonify(Event.query.all())
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = Event.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
@@ -52,7 +69,7 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    task = Event.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form['content']
